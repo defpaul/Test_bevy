4bccde8fe1e5eae10fd310deabab05478850e279
paul <paul_Obercheker@gmx.de>
2023-04-08 17:08:37 +0200

    add player

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, transform::commands};
 
 pub const CLEAR:Color = Color::rgb(0.1, 0.1, 0.1);
 
@@ -15,16 +15,37 @@ fn main() {
         ..default()
     }))
     .insert_resource(ClearColor(CLEAR))
-    .add_startup_system(setup)
+    .add_startup_system(spawn_cam)
+    .add_startup_system(spawn_player)
 //    .add_system(hello_world)
     .run()
 }
+
 
 fn hello_world() {
     println!("hallo world");
 }
 
-fn setup(mut commands: Commands) {
-    commands
-        .spawn_bundle(OrthographicCameraBundle::new_2d());
+fn spawn_cam(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+
+#[derive(Component)]
+struct Player;
+
+fn spawn_player(
+    mut commands: Commands,
+    mut textture_atlas: ResMut<Assets<TextureAtlas>>,
+    asset_server: Res<AssetServer>
+) {
+    let atlas = TextureAtlas::from_grid(
+        asset_server.load("Characters/character_0001.png"),
+        Vec2::splat(24.),
+        11, 1, None, None);
+    commands.spawn((SpriteSheetBundle {
+        texture_atlas: textture_atlas.add(atlas),
+        sprite: TextureAtlasSprite { index: 0, ..Default::default() },
+        ..Default::default()
+    }, Player)); 
 }
